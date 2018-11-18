667b96a7d3d1b7d87e942b8dd5abc0fea39e1d5f
cleemullins <cleemullins@users.noreply.github.com>
2018-11-17 19:51:08 -0800

    Running "dotnet msbuild PublishPipelineTesting" now executes the code found in the custom task

---
+++ PublishPipelineTesting/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace PublishPipelineTesting
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
---
+++ PublishPipelineTesting/PublishValidationTask.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Microsoft.Build.Framework;
+
+namespace PublishPipelineTesting
+{
+    public class PublishValidationTask : Microsoft.Build.Utilities.Task
+    {
+        public override bool Execute()
+        {
+            Log.LogMessage(MessageImportance.High, "Hello World");            
+            return false;
+        }
+    }
+}
